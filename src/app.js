9a7f74ef4c0c0eea8fc1cd7a2cfca63e647e9077
lucas lorenzo <luquitas02@gmail.com>
2024-02-19 16:57:00 -0300

    two origin

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ app.use(fileUpload({
 }));
 app.use(cookieParser());
 app.use(cors({
-	origin: ['http://localhost:5173', 'https://65d3ad1f065120266b99eb55--euphonious-praline-e75db5.netlify.app'],
+	origin: ['https://65d3ad1f065120266b99eb55--euphonious-praline-e75db5.netlify.app', 'http://localhost:5173'],
 	methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
 	allowedHeaders: ['Access-Control-Allow-Origin', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
 	credentials: true
