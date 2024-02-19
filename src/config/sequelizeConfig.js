2b14a925dfd27ab470048af810eb817a7844cccd
lucas lorenzo <luquitas02@gmail.com>
2024-02-19 16:12:49 -0300

    change to production db

--- src/config/sequelizeConfig.js
+++ src/config/sequelizeConfig.js
@@ -3,16 +3,16 @@ const pg = require('pg');
 require('dotenv').config();
 
 //DEV CONFIG
-const sequelize = new Sequelize('gestion-socios-club-dev', 'postgres', 'Marruecos02',{
+/* const sequelize = new Sequelize('gestion-socios-club-dev', 'postgres', 'Marruecos02',{
 	dialectModule: pg,
 	host: '127.0.0.1',
 	dialect:  'postgres',
 	//port: process.env.POSTGRESQL_PORT,
 	logging: false
-});   
+});   */ 
 
 //PRODUCTION CONFIG
-/* const sequelize = new Sequelize(process.env.PRODUCTION_DB_CONNECTION_STRING, {
+const sequelize = new Sequelize(process.env.PRODUCTION_DB_CONNECTION_STRING, {
 	dialectModule: pg,
 	dialect:  'postgres',
 	port: process.env.POSTGRESQL_PORT,
@@ -23,6 +23,6 @@ const sequelize = new Sequelize('gestion-socios-club-dev', 'postgres', 'Marrueco
 			rejectUnauthorized: false,
 		},
 	},
-});   */
+});
 
 module.exports = sequelize;
