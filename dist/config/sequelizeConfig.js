eae951953649f0db607910841ea420b28f2f8a3c
lucas lorenzo <luquitas02@gmail.com>
2024-03-04 11:48:26 -0300

    change database

--- dist/config/sequelizeConfig.js
+++ dist/config/sequelizeConfig.js
@@ -3,18 +3,17 @@ import pg from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
 //DEV CONFIG
-export default new Sequelize('gestion-socios-club-dev', 'postgres', 'Marruecos02', {
+/* export default new Sequelize('gestion-socios-club-dev', 'postgres', 'Marruecos02',{
     dialectModule: pg,
     host: '127.0.0.1',
-    dialect: 'postgres',
+    dialect:  'postgres',
     //port: process.env.POSTGRESQL_PORT,
     logging: false
-});
+});   */
 //PRODUCTION CONFIG
-/* const sequelize = new Sequelize(process.env.PRODUCTION_DB_CONNECTION_STRING, {
+export default new Sequelize(process.env.PRODUCTION_DB_CONNECTION_STRING, {
     dialectModule: pg,
-    dialect:  'postgres',
-    port: process.env.POSTGRESQL_PORT,
+    dialect: 'postgres',
     logging: false,
     dialectOptions: {
         ssl: {
@@ -22,5 +21,5 @@ export default new Sequelize('gestion-socios-club-dev', 'postgres', 'Marruecos02
             rejectUnauthorized: false,
         },
     },
-}); */ 
+});
 //# sourceMappingURL=sequelizeConfig.js.map
--- src/config/sequelizeConfig.ts
+++ src/config/sequelizeConfig.ts
@@ -4,20 +4,19 @@ import dotenv from 'dotenv';
 dotenv.config(); 
 
 //DEV CONFIG
-export default new Sequelize('gestion-socios-club-dev', 'postgres', 'Marruecos02',{
+/* export default new Sequelize('gestion-socios-club-dev', 'postgres', 'Marruecos02',{
 	dialectModule: pg,
 	host: '127.0.0.1',
 	dialect:  'postgres',
 	//port: process.env.POSTGRESQL_PORT,
 	logging: false
-});  
+});   */
 
 //PRODUCTION CONFIG
 
-/* const sequelize = new Sequelize(process.env.PRODUCTION_DB_CONNECTION_STRING, {
+export default new Sequelize(process.env.PRODUCTION_DB_CONNECTION_STRING, {
 	dialectModule: pg,
 	dialect:  'postgres',
-	port: process.env.POSTGRESQL_PORT,
 	logging: false,
 	dialectOptions: {
 		ssl: {
@@ -25,4 +24,4 @@ export default new Sequelize('gestion-socios-club-dev', 'postgres', 'Marruecos02
 			rejectUnauthorized: false,
 		},
 	},
-}); */
+}); 
